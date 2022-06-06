f249f2bc1576dde304e8c645aade72069fbecda4
Alexandr <104072970+Dennchik@users.noreply.github.com>
2022-06-06 07:41:34 +0300

    6 июня 2022
    7:44

--- js/side-bar.js
+++ js/side-bar.js
@@ -1,38 +1,37 @@
 //* Side-Bar-Menu - animation
 var sideopen = document.getElementById('sideopen');
-var sidenav = document.getElementById('sidenav');
+var sidenav = document.getElementById('link');
+var sidenav = document.getElementById('content');
 
 sideopen.onmouseenter = function openNav() {
 	document.getElementById("sideopen").style.width = "13rem";
-	document.getElementById("sidenav").style.width = "13rem";
-	document.getElementById("sidenav").style.opacity = "1";
-	document.getElementById("content").style.marginLeft = "13.2rem";
-	document.getElementById("middle").style.marginLeft = "13.2rem";
+	document.getElementById("link").style.opacity = "1";
 
 }
 
 sideopen.onmouseleave = function closeNav() {
 	document.getElementById("sideopen").style.width = "0";
-	document.getElementById("sidenav").style.width = "0";
-	document.getElementById("sidenav").style.opacity = "0";
-	document.getElementById("content").style.marginLeft = "5.3rem";
-	document.getElementById("middle").style.marginLeft = "5.3rem";
+	document.getElementById("link").style.opacity = "0";
 
-}
+	
+};
 
-// //*фиксированный Side-Bar-Menu
-// window.onscroll = function () { myFunction() };
+//* Цикл через все кнопки выпадающего списка для переключения между скрытием и отображением его выпадающего содержимого - это позволяет пользователю иметь несколько выпадающих списков без каких-либо конфликтов
+var dropdown = document.getElementsByClassName("drop-btn");
+var i;
 
-// var sidenav = document.getElementById("sideopen");
-// var sticky = sidenav.offsetTop;
+for (i = 0; i < dropdown.length; i++) {
+	dropdown[i].addEventListener("click", function () {
+		this.classList.toggle("active-link");
+		var dropdownContent = this.nextElementSibling;
+		if (dropdownContent.style.display === "block") {
+			dropdownContent.style.display = "none";
+		} else {
+			dropdownContent.style.display = "block";
+		}
+	});
+};
 
-// function myFunction() {
-// 	if (window.pageYOffset > sticky) {
-// 		sidenav.classList.add("sticky");
-// 	} else {
-// 		sidenav.classList.remove("sticky");
-// 	}
-// }
 
 //* Serch
 $(document).ready(function () {
@@ -63,19 +62,48 @@ $(document).ready(function () {
 	});
 });
 
-//* Цикл через все кнопки выпадающего списка для переключения между скрытием и отображением его выпадающего содержимого - это позволяет пользователю иметь несколько выпадающих списков без каких-либо конфликтов
-var dropdown = document.getElementsByClassName("drop-btn");
-var i;
+// //* Serch second
+$(document).ready(function () {
+	$("input").bind("click", function () {
+		var input = $(this);
 
-for (i = 0; i < dropdown.length; i++) {
-	dropdown[i].addEventListener("click", function () {
-		this.classList.toggle("active-link");
-		var dropdownContent = this.nextElementSibling;
-		if (dropdownContent.style.display === "block") {
-			dropdownContent.style.display = "none";
+		if (input.val() == "Search") {
+			input.val("");
 		} else {
-			dropdownContent.style.display = "block";
+			this.select();
+		}
+
+		$("#wrap-search-second").addClass("active");
+		$(input).parent().removeClass("close").addClass("expand");
+	});
+
+	$("#icn-close").bind("click", function () {
+		$("#wrap-search-second").removeClass("active");
+		$(this).parent().addClass("close").removeClass("expand");
+		$(this).parent().children("input").val("Search");
+	});
+
+	$("input").bind("blur", function () {
+		input = $(this);
+		if (input.val() == "") {
+			$("#icn-close").trigger("click");
 		}
 	});
-}
+});
 
+
+
+
+// //*фиксированный Side-Bar-Menu
+// window.onscroll = function () { myFunction() };
+
+// var sidenav = document.getElementById("sideopen");
+// var sticky = sidenav.offsetTop;
+
+// function myFunction() {
+// 	if (window.pageYOffset > sticky) {
+// 		sidenav.classList.add("sticky");
+// 	} else {
+// 		sidenav.classList.remove("sticky");
+// 	}
+// }
--- src/js/sidebar.js
+++
@@ -1,9 +0,0 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-sideopen.onmouseenter = function openNav() {
-	document.getElementById("sideopen").style.width = "250px";
-	document.getElementById("main").style.marginLeft = "250px";
-}
-sideopen.onmouseleave = function closeNav() {
-	document.getElementById("sideopen").style.width = "0";
-	document.getElementById("main").style.marginLeft = "0";
-}
