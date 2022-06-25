9374a17fa9c0f561f7f51e276afcad446aaad28c
Alexandr <104072970+Dennchik@users.noreply.github.com>
2022-06-25 22:11:26 +0300

    25 июнь 2020
    new

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var massage;
+var myMassage;
+myMassage = "Сообщение";
+massage = " для Васи";
+
+
+var res = (myMassage + massage);
+console.log(res)
--- js/side-bar.js
+++ js/side-bar.js
@@ -1,22 +1,31 @@
+$("#left-nav .left-menu > a").click(function (b) {
+	$("#left-nav ul ul").slideUp(), $(this).next().is(":visible") || $(this).next().slideDown(),
+		b.stopPropagation()
+});
+
+
+
+
 //* Side-Bar-Menu - animation
 var sideopen = document.getElementById('sideopen');
-var sidenav = document.getElementById('link');
-var sidenav = document.getElementById('content');
+var sidenav = document.getElementById('sidebar-link');
+
 
 sideopen.onmouseenter = function openNav() {
-	document.getElementById("sideopen").style.width = "13rem";
-	document.getElementById("link").style.opacity = "1";
+	document.getElementById("sideopen").style.width = "15em";
+	document.getElementById("sidebar-link").style.opacity = "1";
 
 }
 
 sideopen.onmouseleave = function closeNav() {
 	document.getElementById("sideopen").style.width = "0";
-	document.getElementById("link").style.opacity = "0";
+	document.getElementById("sidebar-link").style.opacity = "0";
 
 	
 };
 
 //* Цикл через все кнопки выпадающего списка для переключения между скрытием и отображением его выпадающего содержимого - это позволяет пользователю иметь несколько выпадающих списков без каких-либо конфликтов
+
 var dropdown = document.getElementsByClassName("drop-btn");
 var i;
 
@@ -31,6 +40,25 @@ for (i = 0; i < dropdown.length; i++) {
 		}
 	});
 };
+
+
+function openCity(evt, cityName) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace("select", "");
+	}
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " select";
+}
+
+// //* Получите элемент с id="defaultOpen" и нажмите на него
+document.getElementById("defaultOpen").click();
+
 
 
 //* Serch
@@ -62,48 +90,4 @@ $(document).ready(function () {
 	});
 });
 
-// //* Serch second
-$(document).ready(function () {
-	$("input").bind("click", function () {
-		var input = $(this);
 
-		if (input.val() == "Search") {
-			input.val("");
-		} else {
-			this.select();
-		}
-
-		$("#wrap-search-second").addClass("active");
-		$(input).parent().removeClass("close").addClass("expand");
-	});
-
-	$("#icn-close").bind("click", function () {
-		$("#wrap-search-second").removeClass("active");
-		$(this).parent().addClass("close").removeClass("expand");
-		$(this).parent().children("input").val("Search");
-	});
-
-	$("input").bind("blur", function () {
-		input = $(this);
-		if (input.val() == "") {
-			$("#icn-close").trigger("click");
-		}
-	});
-});
-
-
-
-
-// //*фиксированный Side-Bar-Menu
-// window.onscroll = function () { myFunction() };
-
-// var sidenav = document.getElementById("sideopen");
-// var sticky = sidenav.offsetTop;
-
-// function myFunction() {
-// 	if (window.pageYOffset > sticky) {
-// 		sidenav.classList.add("sticky");
-// 	} else {
-// 		sidenav.classList.remove("sticky");
-// 	}
-// }
